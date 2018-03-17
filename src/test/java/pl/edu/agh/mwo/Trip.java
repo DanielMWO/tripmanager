94e98094933832b1fdda10b54f221845cc983141
student18 <student18@pcoip.ki.agh.edu.pl>
2018-03-17 12:12:37 +0100

    4rd

--- src/test/java/pl/edu/agh/mwo/Trip.java
+++ src/test/java/pl/edu/agh/mwo/Trip.java
@@ -5,7 +5,7 @@ public class Trip {
 	public Photo getPhotos() {
 		// TODO Auto-generated method stub
 		return null;
-	}
+	} 
 
 	public void addPhoto(Photo photo) {
 		// TODO Auto-generated method stub
