dc390d82d34a2e26c4b6a69f36b863f513c7535e
student18 <student18@pcoip.ki.agh.edu.pl>
2018-03-17 11:56:37 +0100

    Test1 commit

--- src/test/java/pl/edu/agh/mwo/TripManagerTest.java
+++ src/test/java/pl/edu/agh/mwo/TripManagerTest.java
@@ -1,5 +1,19 @@
 package pl.edu.agh.mwo;
- 
+
+import org.junit.Assert.*;
+
 public class TripManagerTest {
+	
+	@Test
+	public void addPhotoTest {
+		Trip trip = new Trip();
+		Photo photo = new Photo();
+		assertEguals(0, trip.getPhotos().size());
+		trip.addPhoto(photo);
+		assertEguals(1, trip.getPhotos().size());
+		
+		
+		
+	}
 	
 }
